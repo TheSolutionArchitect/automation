17340e9138a49dc7fe7c6da3bfd89c2dfcebf077
AWSTechGuide <info@awstechguide.com>
2020-05-11 10:11:54 -0400

    findby text

--- selenium-htmlunitdriver/src/test/java/com/awstechguide/automation/AppTest.java
+++ selenium-htmlunitdriver/src/test/java/com/awstechguide/automation/AppTest.java
@@ -1,6 +1,9 @@
 package com.awstechguide.automation;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.testng.annotations.Test;
 
@@ -10,6 +13,7 @@ import org.testng.annotations.Test;
 public class AppTest 
 {
 	String URL ="https://www.amazon.ca";
+	String URL2 ="https://awstechguide.com/";
 	public HtmlUnitDriver driver;
 	
 	@Test
@@ -19,4 +23,14 @@ public class AppTest
 		   driver.findElement(By.xpath("//input[@type='text']")).sendKeys("surface pro 7");
 		   driver.findElement(By.xpath("//input[@type='submit']")).click();
 	   }
+	
+	@Test
+	public void testURL2() {
+		   driver = new HtmlUnitDriver();
+		   driver.navigate().to(URL2);
+		   List<WebElement> el= driver.findElements(By.xpath("//*[text()[contains(.,'AWS Tech')]]"));
+		   System.out.println("No of existance: "+el.size());
+	   }
+	
+	
 }
