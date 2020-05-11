eae002be2af9019f1c33f15705f2e86bf91b7651
AWSTechGuide <info@awstechguide.com>
2020-05-11 10:35:50 -0400

    new methods

--- selenium-htmlunitdriver/src/test/java/com/awstechguide/automation/AppTest.java
+++ selenium-htmlunitdriver/src/test/java/com/awstechguide/automation/AppTest.java
@@ -1,10 +1,13 @@
 package com.awstechguide.automation;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
@@ -15,8 +18,9 @@ public class AppTest
 	String URL ="https://www.amazon.ca";
 	String URL2 ="https://awstechguide.com/";
 	public HtmlUnitDriver driver;
+	public WebDriverWait wait;
 	
-	@Test
+	@Test(priority = 0)
 	   public void testURL() {
 		   driver = new HtmlUnitDriver();
 		   driver.navigate().to(URL);
@@ -24,7 +28,7 @@ public class AppTest
 		   driver.findElement(By.xpath("//input[@type='submit']")).click();
 	   }
 	
-	@Test
+	@Test(priority = 1)
 	public void testURL2() {
 		   driver = new HtmlUnitDriver();
 		   driver.navigate().to(URL2);
@@ -32,6 +36,30 @@ public class AppTest
 		   System.out.println("No of existance: "+el.size());
 		   driver.findElements(By.partialLinkText("Tech"));
 	   }
+	
+    public void test3()  {
+		driver = new HtmlUnitDriver();
+		wait = new WebDriverWait(driver, 5);
+		driver.get(URL);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		String getTitle = driver.getTitle();
+		Assert.assertEquals(getTitle, "Google");
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Som Speaks");;
+		driver.findElement(By.xpath("//input[@value='Google Search']")).click();;
+		System.out.println("getTitle*********"+getTitle);
+		driver.quit();
+    }
+	
+
+    public void test4()  {
+		driver = new HtmlUnitDriver();
+		wait = new WebDriverWait(driver, 5);
+		driver.get(URL2);
+		String getTitle = driver.getTitle();
+		Assert.assertEquals(getTitle, "AWS Tech Guide");
+		System.out.println("getTitle*********"+getTitle);
+		driver.quit();
+	}
 	
 	
 }
